1909e1a796c342f21c84e711198ca06c9c568b9d
Stephan Kern <stephan@swmmrman.com>
2022-10-10 18:44:47 -0600

    First response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+"<html>
+    <body>
+        <h1>Hellow World</h1>
+    </body>
+</html>"
+}
+
+#[launch]
+fn rocket( ) -> _ {
+    rocket::build().mount("/", routes![index])
 }

40f351957efa182f6e0da7369676aaf588525c72
Stephan Kern <stephan@swmmrman.com>
2022-10-10 22:51:59 -0600

    Playing with async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rocket::tokio::time::{sleep, Duration};
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -14,9 +15,16 @@ fn welcome() -> String {
     std::fs::read_to_string("tmpl.html").unwrap()
 }
 
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Slept for {}", seconds)
+}
+
 #[launch]
 fn rocket( ) -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![welcome])
+        .mount("/", routes![delay])
 }
